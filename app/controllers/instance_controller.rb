3ca4bfdbc4fe57af068f0cdea1da57e72d1e900a
Philihp Busby <philihp@gmail.com>
2016-08-22 02:30:30 -0700

    add create to instance

--- app/controllers/instance_controller.rb
+++ app/controllers/instance_controller.rb
@@ -1,4 +1,7 @@
 class InstanceController < ApplicationController
+  def create
+  end
+
   def show
   end
 end
--- test/controllers/instance_controller_test.rb
+++ test/controllers/instance_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class InstanceControllerTest < ActionDispatch::IntegrationTest
+  test "should get create" do
+    get instance_create_url
+    assert_response :success
+  end
+
   test "should get show" do
     get instance_show_url
     assert_response :success
