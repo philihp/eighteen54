aaa7db26b592411fcb79bc045d673f362f7eff4c
Philihp Busby <philihp@gmail.com>
2016-08-24 01:42:14 -0700

    Prevent game from starting with wrong players

--- app/models/instance_in_setup.rb
+++ app/models/instance_in_setup.rb
@@ -1,6 +1,14 @@
 class InstanceInSetup < Instance
 
+  class TooFewPlayersError < StandardError
+  end
+
+  class TooManyPlayersError < StandardError
+  end
+
   def bump_round!
+    raise TooFewPlayersError if players.size < 3
+    raise TooManyPlayersError if players.size > 6
     self.auction!
   end
 
