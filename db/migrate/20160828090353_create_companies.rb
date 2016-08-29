5605f8e9b33bdaeecee9314f9a17c68e7bddd660
Philihp Busby <philihp@gmail.com>
2016-08-29 02:41:27 -0700

    Refernece table

--- db/migrate/20160828090353_create_companies.rb
+++ db/migrate/20160828090353_create_companies.rb
@@ -2,7 +2,7 @@ class CreateCompanies < ActiveRecord::Migration[5.0]
   def change
     create_table :companies do |t|
       t.references :instance, foreign_key: true
-      t.references :director, foreign_key: true
+      t.references :director, foreign_key: true, table_name: :players
       t.string :type
       t.timestamps
     end
